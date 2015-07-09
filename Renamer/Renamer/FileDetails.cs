aef98a2820e6062e60e701dfd20d4bfa3ad78443
Wellodian <Wellodian@users.noreply.github.com>
2015-07-09 18:10:20 +0930

    changes

---
+++ Renamer/Renamer/FileDetails.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Renamer
+{
+    class FileDetails
+    {
+
+        public FileDetails(string directory, string fileParam) {
+        
+            DirectoryName = directory;
+            FileName = fileParam;
+
+        }
+
+
+        private string directoryName;
+
+        public string DirectoryName
+        {
+            get { return directoryName; }
+            set { directoryName = value; }
+        }
+        private string fileName;
+
+        public string FileName
+        {
+            get { return fileName; }
+            set { fileName = value; }
+        }
+
+    }
+}
